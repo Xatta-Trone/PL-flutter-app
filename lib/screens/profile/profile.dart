cd44e5a17ea305820c8183704ac0f13d96aa6767
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2023-05-03 00:36:49 +0900

    optional removal of callout text

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -7,9 +6,44 @@ import 'package:plandroid/constants/const.dart';
 import 'package:plandroid/controller/AuthController.dart';
 import 'package:plandroid/routes/routeconst.dart';
 import 'package:plandroid/screens/auth/Login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  bool _dismissText = false;
+
+  void getDismissTextStatus() async {
+    SharedPreferences preference = await SharedPreferences.getInstance();
+    var value = preference.getBool('_dismissText');
+
+    if (value != null) {
+      setState(() {
+        _dismissText = value;
+      });
+    }
+  }
+
+  void setDismissTextStatus() async {
+    SharedPreferences preference = await SharedPreferences.getInstance();
+
+    preference.setBool('_dismissText', true);
+
+    setState(() {
+      _dismissText = true;
+    });
+  }
+
+  @override
+  void initState() {
+    getDismissTextStatus();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,13 +86,11 @@ class Profile extends StatelessWidget {
                                 child: Text(
                                   authController.user.value?.user.userLetter ??
                                       '',
-                                  style: theme
-                                      .textTheme
-                                      .headlineMedium
-                                      ?.copyWith(
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.white,
-                                      ),
+                                  style:
+                                      theme.textTheme.headlineMedium?.copyWith(
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                  ),
                                 ),
                               ),
                             ),
@@ -67,30 +99,49 @@ class Profile extends StatelessWidget {
                             ),
                             Text(
                               "#${authController.user.value?.user.studentId ?? ''}",
-                              style: theme
-                                  .textTheme
-                                  .headlineMedium
-                                  ?.copyWith(
-                                    fontWeight: FontWeight.w400,
-                                    color: Colors.white,
-                                  ),
+                              style: theme.textTheme.headlineMedium?.copyWith(
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white,
+                              ),
                             ),
                           ],
                         ),
                       ),
                     ),
                     divider,
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 15.0, vertical: 10.0),
-                      child: Text(
-                          'Please refrain from sharing your account with others. Such activity may lead to permanent account suspension. The website constantly monitors all activities to prevent account sharing. You can see your activities from the Activity section.',
-                          textScaleFactor: 0.85,
-                          softWrap: true,
-                          style: theme.textTheme.titleMedium
-                              ?.copyWith(color: Colors.redAccent)),
-                    ),
-                    
+                    if (_dismissText == false) ...[
+                      Dismissible(
+                        key: UniqueKey(),
+                        onDismissed: (DismissDirection direction) {
+                          setDismissTextStatus();
+                          if (kDebugMode) {
+                            print(direction);
+                          }
+                        },
+                        child: ListTile(
+                          trailing: const SizedBox(
+                            height: double.infinity,
+                            child: Icon(
+                              FontAwesomeIcons.arrowRightLong,
+                            ),
+                          ),
+                          leading: const SizedBox(
+                            height: double.infinity,
+                            child: Icon(
+                              FontAwesomeIcons.arrowLeftLong,
+                            ),
+                          ),
+                          title: Text(
+                            'Please refrain from sharing your account with others. Such activity may lead to permanent account suspension. The website constantly monitors all activities to prevent account sharing. You can see your activities from the Activity section.',
+                            textScaleFactor: 0.8,
+                            textAlign: TextAlign.justify,
+                            softWrap: true,
+                            style: theme.textTheme.titleMedium
+                                ?.copyWith(color: Colors.redAccent),
+                          ),
+                        ),
+                      )
+                    ],
                     divider,
                     ListTile(
                       leading: const Icon(
@@ -106,7 +157,6 @@ class Profile extends StatelessWidget {
                     ListTile(
                       leading: const Icon(
                         FontAwesomeIcons.envelope,
-                        
                       ),
                       title: Text(
                         authController.user.value?.user.email ?? '',
@@ -118,7 +168,6 @@ class Profile extends StatelessWidget {
                     ListTile(
                       leading: const FaIcon(
                         FontAwesomeIcons.calendarDays,
-                        
                       ),
                       title: Text(
                         authController.getCreateAt(),
@@ -126,17 +175,14 @@ class Profile extends StatelessWidget {
                         softWrap: true,
                       ),
                     ),
-
                     divider,
                     ListTile(
                       leading: const FaIcon(
                         FontAwesomeIcons.circlePlus,
-                        
                       ),
                       title: const Text('Contribute materials'),
                       trailing: const FaIcon(
                         FontAwesomeIcons.chevronRight,
-                        
                         size: iconSize,
                       ),
                       onTap: () {
@@ -150,12 +196,10 @@ class Profile extends StatelessWidget {
                     ListTile(
                       leading: const FaIcon(
                         FontAwesomeIcons.chartColumn,
-                        
                       ),
                       title: const Text('Activity log'),
                       trailing: const FaIcon(
                         FontAwesomeIcons.chevronRight,
-                        
                         size: iconSize,
                       ),
                       onTap: () {
@@ -166,12 +210,10 @@ class Profile extends StatelessWidget {
                     ListTile(
                       leading: const FaIcon(
                         FontAwesomeIcons.houseLaptop,
-                        
                       ),
                       title: const Text('Device log'),
                       trailing: const FaIcon(
                         FontAwesomeIcons.chevronRight,
-                        
                         size: iconSize,
                       ),
                       onTap: () {
@@ -185,12 +227,10 @@ class Profile extends StatelessWidget {
                     ListTile(
                       leading: const FaIcon(
                         FontAwesomeIcons.laptopFile,
-                        
                       ),
                       title: const Text('Saved devices'),
                       trailing: const FaIcon(
                         FontAwesomeIcons.chevronRight,
-                        
                         size: iconSize,
                       ),
                       onTap: () {
@@ -200,17 +240,14 @@ class Profile extends StatelessWidget {
                         Get.toNamed(userListedDevices);
                       },
                     ),
-                    
                     divider,
                     ListTile(
                       leading: const FaIcon(
                         FontAwesomeIcons.lock,
-                        
                       ),
                       title: const Text('Change password'),
                       trailing: const FaIcon(
                         FontAwesomeIcons.chevronRight,
-                        
                         size: iconSize,
                       ),
                       onTap: () {
@@ -221,17 +258,13 @@ class Profile extends StatelessWidget {
                       },
                     ),
                     divider,
-                    
-
                     ListTile(
                         leading: const Icon(
                           FontAwesomeIcons.rightFromBracket,
-                          
                         ),
                         title: const Text('Logout'),
                         trailing: const FaIcon(
                           FontAwesomeIcons.chevronRight,
-                          
                           size: iconSize,
                         ),
                         onTap: () {
@@ -263,8 +296,6 @@ class Profile extends StatelessWidget {
                             },
                           );
                         }),
-                   
-                    
                   ],
                 ),
               ),
